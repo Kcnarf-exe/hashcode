ffba436b963941a1880aad4ab63048a86eea1048
Sobolevsky.A <Andrey.Sobolevsky@ulb.ac.be>
2021-02-24 20:43:23 +0100

    ok

--- train2/Table.cpp
+++ train2/Table.cpp
@@ -1,7 +1,8 @@
 #include "Table.h"
 
-Table::Table(string inputFile) {
-    ifstream file(inputFile);
+Table::Table(string inputFile)
+{
+  /*ifstream file(inputFile);
     string firstLine;
     getline(file, firstLine);
     vector<string> infoFirst = split(firstLine, ' ');
@@ -19,19 +20,23 @@ Table::Table(string inputFile) {
         info.erase(info.begin());
         pizza = new Pizza(count, info);
         this->pizzas.push_back(pizza);
-    }
-    
+    }*/
 
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  freopen(inputFile, "r", stdin);
 }
 
-vector<string> Table::split(string str, char delimiter) { 
-  vector<string> internal; 
-  stringstream ss(str); // Turn the string into a stream. 
-  string tok; 
- 
-  while(getline(ss, tok, delimiter)) { 
-    internal.push_back(tok); 
-  } 
- 
-  return internal; 
+vector<string> Table::split(string str, char delimiter)
+{
+  vector<string> internal;
+  stringstream ss(str); // Turn the string into a stream.
+  string tok;
+
+  while (getline(ss, tok, delimiter))
+  {
+    internal.push_back(tok);
+  }
+
+  return internal;
 }
--- train2/Table.h
+++ train2/Table.h
@@ -2,21 +2,23 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <bits/stdc++.h>
 
 #include "Pizza.h"
 
 using namespace std;
 
-class Table {
-    private:
-        string inputFile;
-        int M;
-        int T2;
-        int T3;
-        int T4;
-        vector<Pizza*> pizzas;
-    public :
-        Table(string inputFile);
-        vector<string> split(string str, char delimiter);
+class Table
+{
+private:
+    string inputFile;
+    int M;
+    int T2;
+    int T3;
+    int T4;
+    vector<Pizza *> pizzas;
 
+public:
+    Table(string inputFile);
+    vector<string> split(string str, char delimiter);
 }
