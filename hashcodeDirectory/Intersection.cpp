001ebf7a7fc11809e4a0a4b3f4e2bd1cb61059e8
Kcnarf-exe <francktrouillez99@gmail.com>
2021-02-25 19:34:08 +0100

    modif

--- hashcodeDirectory/Intersection.cpp
+++ hashcodeDirectory/Intersection.cpp
@@ -9,4 +9,14 @@ void Intersection::changeGreenLightId(int id){
     this->currentGreenLightId = id;
 }
 
-int Intersection::getGreenLightId();
+int Intersection::getGreenLightId(){
+    return this->currentGreenLightId;
+}
+
+void Intersection::addGreenLightSchedule(int inputStreetId, int duration) {
+    this->schedule.push_back(make_pair(inputStreetId, duration));
+}
+
+vector<pair<int,int>> Intersection::getSchedule() {
+    return schedule;
+}
