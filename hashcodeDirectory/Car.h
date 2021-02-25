67164b508fd17200fcd0e2b9e5b3942c5cf6c148
neji601 <>
2021-02-25 19:39:36 +0100

    CAr modif

--- hashcodeDirectory/Car.h
+++ hashcodeDirectory/Car.h
@@ -7,8 +7,20 @@ class Car {
         int numberOfStreets;
         vector<int> listOfStreets;
 
+        //Variable pour la simu
+        int timeTransition; //Si =0 elle est a l intersection a la fin de la rue actuel
+        int idStreetIsIn; //Id of the street where it is now
+
     public :
+        Car(int numberOfStreets, vector<int> listStreets);
         int getNumberOfStreets();
         vector<int> getListOfStreets();
+
+        int getIdStreet();
+        void setIdStreet(int id);
+
+        int getTimeTransition();
+        void setTimeTransition(int time);
+        void decrementTimeTransition(); //Decrement the timer on the moving car, if <= the it does not move
     
 };
